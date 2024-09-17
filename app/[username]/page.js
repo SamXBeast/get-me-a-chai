e1206d2c63316a9e0ab3db6a95c6da57a89cbaf9
SAMYAK MAHAPATRA <145669954+SamXBeast@users.noreply.github.com>
2024-09-17 20:09:46 +0530

    Fixture1

---
+++ app/[username]/page.js
@@ -0,0 +1,25 @@
+import PaymentPage from "@/components/PaymentPage";
+import React from "react";
+import { notFound } from "next/navigation";
+import connectDb from "@/db/connectDb";
+import User from "@/models/User";
+
+const username = async ({ params }) => {
+  const checkUser = async () => {
+    await connectDb();
+    let u = await User.findOne({ username: params.username });
+    if (!u) {
+      return notFound();
+    }
+  };
+
+  await checkUser();
+
+  return (
+    <>
+      <PaymentPage username={params.username} />
+    </>
+  );
+};
+
+export default username;
